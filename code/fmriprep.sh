69bb27b608194e5076d048d443cdfb5a2a9a223f
David V. Smith <david.v.smith@temple.edu>
2020-12-29 01:33:33 -0500

    update paths and version of fmriprep

--- code/fmriprep.sh
+++ code/fmriprep.sh
@@ -1,25 +1,27 @@
 # example code for FMRIPREP
 # runs FMRIPREP on input subject
-# usage: bash run_fmriprep.sh sub
-# example: bash run_fmriprep.sh 102
+# usage: bash fmriprep.sh sub
+# example: bash fmriprep.sh 102
 
 sub=$1
 
-# set up input and output directories.
-maindir=`pwd` # assume you are running from the root
+# ensure paths are correct irrespective from where user runs the script
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+maindir="$(dirname "$scriptdir")"
 
-# make derivatives folder if it doesn't exist. 
-# let's keep this out of bids for now
+# bids directory (from Bart: https://github.com/klabhub/ds.tacsCardGame)
+bidsdir=/data/projects/ds.tacsCardGame
+
+# make derivatives and scratch folders if they do not exist.
 if [ ! -d $maindir/derivatives ]; then
 	mkdir -p $maindir/derivatives
 fi
-
 scratchdir=/data/scratch/`whoami`
 if [ ! -d $scratchdir ]; then
 	mkdir -p $scratchdir
 fi
 
-singularity run --cleanenv -B $maindir:/base -B /data/tools/licenses:/opts -B $scratchdir:/scratch \
+singularity run --cleanenv -B $bidsdir:/base -B /data/tools/licenses:/opts -B $scratchdir:/scratch \
 /data/tools/fmriprep-20.1.0.simg \
 /base/bids /base/derivatives \
 participant --participant_label $sub \
