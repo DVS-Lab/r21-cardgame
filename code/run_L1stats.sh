f6055007aeffd19a08d3225abc5dd712e6268fa0
DVSneuro <david.v.smith@temple.edu>
2020-12-30 16:01:09 -0500

    fix error in while loop

--- code/run_L1stats.sh
+++ code/run_L1stats.sh
@@ -3,7 +3,7 @@
 # ensure paths are correct irrespective from where user runs the script
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 basedir="$(dirname "$scriptdir")"
-logs=$maindir/logs
+logs=$basedir/logs
 
 # remove previous log. this is mainly useful when re-running everything to check for completion
 rm -rf ${logs}/re-runL1.log
@@ -13,12 +13,12 @@ for ppi in 0 TPJ VLPFC ecn dmn; do # putting 0 first will indicate "activation"
 
 	# loops through the subject/run list
 	cat ${scriptdir}/runcount.tsv |
-	while read $subrun; do
-		set -- $subrun
+	while read subrun; do
+		set -- ${subrun}
 		sub=$1
 		nruns=$2
 
-		for run in `seq $nruns`; do
+		for run in `seq ${nruns}`; do
 	  	# Manages the number of jobs and cores
 	  	SCRIPTNAME=${basedir}/code/L1stats.sh
 	  	NCORES=25
