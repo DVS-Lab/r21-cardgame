3eda75da8e275e46ad4820002c07bf2bf2f52b75
David V. Smith <david.v.smith@temple.edu>
2021-01-17 21:04:35 -0500

    correct typo in for loop

--- code/run_L3stats.sh
+++ code/run_L3stats.sh
@@ -14,7 +14,7 @@ rm -rf $logs/re-runL3.log
 
 # this loop defines the different types of analyses that will go into the group comparisons
 # for analysistype in type-nppi-dmn type-nppi-ecn type-act type-ppi_seed-TPJ type-ppi_seed-VLPFC; do
-for analysisinfo in type-ppi_seed-bilateralVLPFC type-ppi_seed-leftVLPFC type-ppi_seed-rightVLPFC type-ppi_seed-preCun type-ppi_seed-bilateralVS type-ppi_seed-rightVS type-ppi_seed-leftVS; do
+for analysistype in type-ppi_seed-bilateralVLPFC type-ppi_seed-leftVLPFC type-ppi_seed-rightVLPFC type-ppi_seed-preCun type-ppi_seed-bilateralVS type-ppi_seed-rightVS type-ppi_seed-leftVS; do
 
 	# these define the cope number (copenum) and cope name (copename)
 	for copeinfo in "1 R_vlpfc" "2 R_tpj" "3 P_vlpfc" "4 P_tpj" "5 R-P" "6 vlpfc-tpj" "7 R_vlpfc-tpj" "8 P_vlpfc-tpj" "9 interaction" "10 phys" "11 ppi_R_vlpfc" "12 ppi_R_tpj" "13 ppi_P_vlpfc" "14 ppi_P_tpj" "15 ppi_R-P" "16 ppi_vlpfc-tpj" "17 ppi_R_vlpfc-tpj" "18 ppi_P_vlpfc-tpj" "19 ppi_interaction"; do
