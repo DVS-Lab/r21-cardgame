4b3f44740f8aa343c456bdb9b8f30df9bc598e78
David V. Smith <david.v.smith@temple.edu>
2021-02-09 15:00:43 -0500

    preliminary figures and data sharing

--- code/extractROI.sh
+++ code/extractROI.sh
@@ -1,24 +1,44 @@
 #!/usr/bin/env bash
 
-# make everything relative to root of the GitHub repository
-cd ..
-maindir=`pwd`
+# ensure paths are correct irrespective from where user runs the script
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+maindir="$(dirname "$scriptdir")"
 
-ROI=$1
+# ROI name and other path information
+for ROI in bilateralVLPFC leftVLPFC rightVLPFC bilateralVS leftVS rightVS; do
+	MASK=${maindir}/masks/${ROI}.nii.gz
+	TASK=cardgame
+	TYPE=act
+	outputdir=${maindir}/derivatives/imaging_plots
+	mkdir -p $outputdir
 
-MASK=${maindir}/masks/${ROI}.nii.gz #NAcc_resliced or PreCun_func
-TASK=cardgame
-TYPE=ppi
+	# for COPENUM in 11 12 13 14; do
+	for COPENUM in 1 2 3 4; do
 
-for COPENUM in 11 12 13 14; do
-#for COPENUM in 1 2 3 4; do
+		cnum_padded=`zeropad ${COPENUM} 2`
+		MAINOUTPUT=${maindir}/derivatives/fsl/L3_model-01_task-cardgame_n28_FLAME1+2
+		OUTPUT=${MAINOUTPUT}/L3_task-${TASK}_type-${TYPE}_cnum-${cnum_padded}_*
+		DATA=`ls -1 ${MAINOUTPUT}/L3_task-${TASK}_type-${TYPE}_cnum-${cnum_padded}_*.gfeat/cope1.feat/filtered_func_data.nii.gz`
+		fslmeants -i $DATA -o ${outputdir}/${ROI}_type-${TYPE}_cope-${cnum_padded}.txt -m ${MASK}
 
-	cnum_padded=`zeropad ${COPENUM} 2`
+	done
+done
 
-	MAINOUTPUT=${maindir}/derivatives/fsl/L3_n27_model-01_FLAME1+2_exclusions
-	OUTPUT=${MAINOUTPUT}/L3_task-${TASK}_model-01_type-${TYPE}_cope-${cnum_padded}
-	DATA=${OUTPUT}.gfeat/cope1.feat/filtered_func_data.nii.gz
-	fslmeants -i $DATA -o ${ROI}_type-${TYPE}_cope-${cnum_padded}.txt -m ${MASK}
+# ROI name and other path information
+for ROI in DMNstriatal_conn; do
+	MASK=${maindir}/masks/${ROI}.nii.gz
+	TASK=cardgame
+	TYPE=nppi-dmn
+	outputdir=${maindir}/derivatives/imaging_plots
+	mkdir -p $outputdir
 
+	for COPENUM in 11 12 13 14; do
 
+		cnum_padded=`zeropad ${COPENUM} 2`
+		MAINOUTPUT=${maindir}/derivatives/fsl/L3_model-01_task-cardgame_n28_FLAME1+2 # seed-based controls end with _ppi
+		# L3_task-cardgame_type-nppi-dmn_cnum-17_cname-ppi_R_vlpfc-tpj_onegroup.gfeat
+		DATA=`ls -1 ${MAINOUTPUT}/L3_task-${TASK}_type-${TYPE}_cnum-${cnum_padded}_*.gfeat/cope1.feat/filtered_func_data.nii.gz`
+		fslmeants -i $DATA -o ${outputdir}/${ROI}_type-${TYPE}_cope-${cnum_padded}.txt -m ${MASK}
+
+	done
 done
--- code/prelimROIanalyses.m
+++ code/prelimROIanalyses.m
@@ -1,120 +1,55 @@
 clear; close all;
-load sfn_subs.mat
+
+% set up dirs
+codedir = pwd; % must run from code, so this is not a good solution
+cd ..
+maindir = pwd;
+roidir = fullfile(maindir,'derivatives','imaging_plots');
+
+% loop through rois for activation
+rois = {'bilateralVLPFC', 'leftVLPFC', 'rightVLPFC', 'bilateralVS', 'leftVS', 'rightVS'};
+for r = 1:length(rois)
+    roi = rois{r};
+    
+    c1 = load(fullfile(roidir,[roi '_type-act_cope-01.txt']));
+    c2 = load(fullfile(roidir,[roi '_type-act_cope-02.txt']));
+    c3 = load(fullfile(roidir,[roi '_type-act_cope-03.txt']));
+    c4 = load(fullfile(roidir,[roi '_type-act_cope-04.txt']));
+    reward = [c1 c2];
+    punish = [c3 c4];
+    figure, barweb_dvs2([mean(reward); mean(punish)],[std(reward)/sqrt(length(reward)); std(punish)/sqrt(length(punish)) ])
+    axis square
+    outname = fullfile(maindir,'derivatives','imaging_plots',['act_' roi ]);
+    cmd = ['print -depsc ' outname];
+    eval(cmd);
+    
+end
 
 
-roi = 'PCC_func_cope-17_n27'; % ppi reward: vlpfc > tpj
-c1 = load([roi '_type-ppi_cope-11.txt']);
-c2 = load([roi '_type-ppi_cope-12.txt']);
-c3 = load([roi '_type-ppi_cope-13.txt']);
-c4 = load([roi '_type-ppi_cope-14.txt']);
-reward = [c1 c2];
-punish = [c3 c4];
-figure, barweb_dvs2([mean(reward); mean(punish)],[std(reward)/sqrt(length(reward)); std(punish)/sqrt(length(punish)) ])
-axis square
-cmd = ['print -depsc ppi_' roi];
-eval(cmd);
+% loop through rois for nppi
+rois = {'DMNstriatal_conn'};
+for r = 1:length(rois)
+    roi = rois{r};
+    
+    c1 = load(fullfile(roidir,[roi '_type-nppi-dmn_cope-11.txt']));
+    c2 = load(fullfile(roidir,[roi '_type-nppi-dmn_cope-12.txt']));
+    c3 = load(fullfile(roidir,[roi '_type-nppi-dmn_cope-13.txt']));
+    c4 = load(fullfile(roidir,[roi '_type-nppi-dmn_cope-14.txt']));
+    reward = [c1 c2];
+    punish = [c3 c4];
+    figure, barweb_dvs2([mean(reward); mean(punish)],[std(reward)/sqrt(length(reward)); std(punish)/sqrt(length(punish)) ])
+    axis square
+    outname = fullfile(maindir,'derivatives','imaging_plots',['nppiDMN_' roi ]);
+    cmd = ['print -depsc ' outname];
+    eval(cmd);
+    
+end
 
-% R_VLPFC = c1;
-% R_TPJ = c2;
-% P_VLPFC = c3;
-% P_TPJ = c4;
-
-filename = ['summary_roi-' roi '.tsv'];
-fid = fopen(filename, 'wt');
-fprintf(fid, '%s\t%s\t%s\t%s\t%s\n', 'sub-num','Reward_VLPFC','Reward_TPJ','Punish_VLPFC','Punish_VLPFC');  % header
-fclose(fid);
-dlmwrite(filename,[goodsubs c1 c2 c3 c4],'delimiter','\t','precision','%f','-append');
-
-
-
-
-
-roi = 'leftVS_func_cope-16_n27'; % ppi vlpfc > tpj
-c1 = load([roi '_type-ppi_cope-11.txt']);
-c2 = load([roi '_type-ppi_cope-12.txt']);
-c3 = load([roi '_type-ppi_cope-13.txt']);
-c4 = load([roi '_type-ppi_cope-14.txt']);
-reward = [c1 c2];
-punish = [c3 c4];
-figure, barweb_dvs2([mean(reward); mean(punish)],[std(reward)/sqrt(length(reward)); std(punish)/sqrt(length(punish)) ])
-axis square
-cmd = ['print -depsc ppi_' roi];
-eval(cmd);
-
-
-filename = ['summary_roi-' roi '.tsv'];
-fid = fopen(filename, 'wt');
-fprintf(fid, '%s\t%s\t%s\t%s\t%s\n', 'sub-num','Reward_VLPFC','Reward_TPJ','Punish_VLPFC','Punish_VLPFC');  % header
-fclose(fid);
-dlmwrite(filename,[goodsubs c1 c2 c3 c4],'delimiter','\t','precision','%f','-append');
-
-
-
-
-roi = 'TPJ';
-c1 = load([roi '_type-act_cope-01.txt']);
-c2 = load([roi '_type-act_cope-02.txt']);
-c3 = load([roi '_type-act_cope-03.txt']);
-c4 = load([roi '_type-act_cope-04.txt']);
-reward = [c1 c2];
-punish = [c3 c4];
-figure, barweb_dvs2([mean(reward); mean(punish)],[std(reward)/sqrt(length(reward)); std(punish)/sqrt(length(punish)) ])
-axis square
-cmd = ['print -depsc act_' roi];
-eval(cmd);
-
-
-filename = ['summary_roi-' roi '.tsv'];
-fid = fopen(filename, 'wt');
-fprintf(fid, '%s\t%s\t%s\t%s\t%s\n', 'sub-num','Reward_VLPFC','Reward_TPJ','Punish_VLPFC','Punish_VLPFC');  % header
-fclose(fid);
-dlmwrite(filename,[goodsubs c1 c2 c3 c4],'delimiter','\t','precision','%f','-append');
-
-
-
-
-roi = 'VLPFC';
-c1 = load([roi '_type-act_cope-01.txt']);
-c2 = load([roi '_type-act_cope-02.txt']);
-c3 = load([roi '_type-act_cope-03.txt']);
-c4 = load([roi '_type-act_cope-04.txt']);
-reward = [c1 c2];
-punish = [c3 c4];
-figure, barweb_dvs2([mean(reward); mean(punish)],[std(reward)/sqrt(length(reward)); std(punish)/sqrt(length(punish)) ])
-axis square
-cmd = ['print -depsc act_' roi];
-eval(cmd);
-
-
-
-filename = ['summary_roi-' roi '.tsv'];
-fid = fopen(filename, 'wt');
-fprintf(fid, '%s\t%s\t%s\t%s\t%s\n', 'sub-num','Reward_VLPFC','Reward_TPJ','Punish_VLPFC','Punish_VLPFC');  % header
-fclose(fid);
-dlmwrite(filename,[goodsubs c1 c2 c3 c4],'delimiter','\t','precision','%f','-append');
-
-
-
-
-roi = 'VS_func_n27';
-c1 = load([roi '_type-act_cope-01.txt']);
-c2 = load([roi '_type-act_cope-02.txt']);
-c3 = load([roi '_type-act_cope-03.txt']);
-c4 = load([roi '_type-act_cope-04.txt']);
-reward = [c1 c2];
-punish = [c3 c4];
-figure, barweb_dvs2([mean(reward); mean(punish)],[std(reward)/sqrt(length(reward)); std(punish)/sqrt(length(punish)) ])
-axis square
-cmd = ['print -depsc act_' roi];
-eval(cmd);
-
-
-
-filename = ['summary_roi-' roi '.tsv'];
-fid = fopen(filename, 'wt');
-fprintf(fid, '%s\t%s\t%s\t%s\t%s\n', 'sub-num','Reward_VLPFC','Reward_TPJ','Punish_VLPFC','Punish_VLPFC');  % header
-fclose(fid);
-dlmwrite(filename,[goodsubs c1 c2 c3 c4],'delimiter','\t','precision','%f','-append');
+% filename = ['summary_roi-' roi '.tsv'];
+% fid = fopen(filename, 'wt');
+% fprintf(fid, '%s\t%s\t%s\t%s\t%s\n', 'sub-num','Reward_VLPFC','Reward_TPJ','Punish_VLPFC','Punish_VLPFC');  % header
+% fclose(fid);
+% dlmwrite(filename,[goodsubs c1 c2 c3 c4],'delimiter','\t','precision','%f','-append');
 
 
 
