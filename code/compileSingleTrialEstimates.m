738201360ba94ad443ede5b5c1bd870c0eec7786
DVSneuro <david.v.smith@temple.edu>
2021-11-11 02:43:10 -0500

    change naming and revise regions

--- code/compileSingleTrialEstimates.m
+++ code/compileSingleTrialEstimates.m
@@ -20,9 +20,9 @@ for i = 1:length(subrun)
         end
         
         
-        % loop through rois (n = 13) and make empty data matrix
-        rois = {'act-leftVLPFC', 'act-leftVS', 'act-rightVLPFC', 'act-rightVS', 'leftVLPFCconn-DLPFC', 'leftVLPFCconn-MPFC', 'leftVLPFCconn-PCC', 'ECNconn-MPFC', 'ECNconn-insula', 'DMNconn-leftVS', 'bilateralVLPFCconn-parietal', 'leftVSconn-visual', 'rightVSconn-DLPFC'};
-        data_mat = zeros(64,13); % 64 trials and 13 rois
+        % loop through rois (n = 11) and make empty data matrix
+        rois = {'act-leftVLPFC', 'act-PCC', 'act-rightVLPFC', 'act-preSMA', 'act-thalamus', 'act-rightParietal', 'leftVLPFCconn-DLPFC', 'leftVLPFCconn-MPFC', 'leftVLPFCconn-PCC', 'leftVSconn-visual', 'rightVSconn-DLPFC'};
+        data_mat = zeros(64,11); % 64 trials and 11 rois
         for rr = 1:length(rois)
             roi = rois{rr};
             roifile = fullfile(maindir,'derivatives','singletrial',['sub-' num2str(sub)],['sub-' num2str(sub) '_run-0' num2str(r) '_mask-' roi '.txt']);
@@ -33,7 +33,7 @@ for i = 1:length(subrun)
         filename = sprintf('sub-%d_run-%d_singletrialestimates.csv',sub,r);
         outfile = fullfile(maindir,'derivatives','singletrial','compiled',filename);
         fid = fopen(outfile, 'wt');
-        header = 'sub,run,trial,act-leftVLPFC,act-leftVS,act-rightVLPFC,act-rightVS,leftVLPFCconn-DLPFC,leftVLPFCconn-MPFC,leftVLPFCconn-PCC,ECNconn-MPFC,ECNconn-insula,DMNconn-leftVS,bilateralVLPFCconn-parietal,leftVSconn-visual,rightVSconn-DLPFC\n';
+        header = 'sub,run,trial,act_leftVLPFC,act_PCC,act_rightVLPFC,act_preSMA,act_thalamus,act_rightParietal,conn_leftVLPFC_DLPFC,conn_leftVLPFC_MPFC,conn_leftVLPFC_PCC,conn_leftVS_visual,conn_rightVS_DLPFC\n';
         fprintf(fid, header);
         fclose(fid);
         trials = 1:64;
