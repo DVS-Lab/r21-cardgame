9aa3909a4f18516e9648b7b73dde0cea78f37aee
DVSneuro <david.v.smith@temple.edu>
2021-11-18 19:20:00 -0500

    only activation

--- code/run_L2stats.sh
+++ code/run_L2stats.sh
@@ -9,8 +9,8 @@ logs=$basedir/logs
 rm -rf ${logs}/re-runL2.log
 
 # analyses we are doing; these define input/output paths in the L2stats.sh script
-#for analysisinfo in "type-nppi-dmn 28" "type-nppi-ecn 28" "type-act 9" "type-ppi_seed-TPJ 19" "type-ppi_seed-VLPFC 19"; do
-for analysisinfo in "type-ppi_seed-bilateralVLPFC 19" "type-ppi_seed-leftVLPFC 19" "type-ppi_seed-rightVLPFC 19" "type-ppi_seed-preCun 19" "type-ppi_seed-bilateralVS 19" "type-ppi_seed-rightVS 19" "type-ppi_seed-leftVS 19"; do
+for analysisinfo in "type-act 9"; do
+#for analysisinfo in "type-ppi_seed-bilateralVLPFC 19" "type-ppi_seed-leftVLPFC 19" "type-ppi_seed-rightVLPFC 19" "type-ppi_seed-preCun 19" "type-ppi_seed-bilateralVS 19" "type-ppi_seed-rightVS 19" "type-ppi_seed-leftVS 19"; do
 
 	set -- ${analysisinfo}
 	analysis=$1
