0f56b90d1f577c7ae5ac2d29cff8c81552b744b1
DVSneuro <david.v.smith@temple.edu>
2021-11-19 22:01:12 -0500

    fix path error

--- code/run_L3stats.sh
+++ code/run_L3stats.sh
@@ -7,7 +7,7 @@
 # ensure paths are correct irrespective from where user runs the script
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 maindir="$(dirname "$scriptdir")"
-logs=$basedir/logs
+logs=$maindir/logs
 logfile=${logs}/rerunL3_date-`date +"%FT%H%M"`.log
 
 # this loop defines the different types of analyses that will go into the group comparisons
