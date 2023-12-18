cf58ef6979870e27f24f3a60556de4a8e0825556
David V. Smith <david.v.smith@temple.edu>
2023-12-17 23:19:10 -0500

    test with newest tsv files

--- code/gen_3col_files.sh
+++ code/gen_3col_files.sh
@@ -9,26 +9,27 @@ logs=$maindir/logs
 sub=$1
 
 # create output folder if it doesn't exist
-baseout=${maindir}/derivatives/fsl/EVfiles
+baseout=${maindir}/derivatives/fsl/EVfiles-check
 output=${baseout}/sub-${sub}
 if [ ! -d $output ]; then
-	mkdir -p $baseout
+	mkdir -p $output
 fi
 
-# base bids directory (from Bart: https://github.com/klabhub/ds.tacsCardGame)
-inputdir=/data/projects/ds.tacsCardGame
+## base bids directory (from Bart: https://github.com/klabhub/ds.tacsCardGame)
+#inputdir=/data/projects/ds.tacsCardGame
+inputdir=$maindir
 
 # count valid runs and append to runcount.tsv file
 nruns=`ls -1 ${inputdir}/bids/sub-${sub}/func/sub-${sub}_task-cardgame_run-0?_events.tsv | wc -l`
 echo -e "$sub\t$nruns" >> ${scriptdir}/runcount.tsv
 for run in `seq $nruns`; do
 	# use tsv file that Bart generated, but check to make sure it isn't empty
-	bartfile=${inputdir}/bids/sub-${sub}/func/sub-${sub}_task-cardgame_run-0${run}_events.tsv
+	bartfile=${inputdir}/bids-tmp/sub-${sub}/func/sub-${sub}_task-cardgame_run-0${run}_events.tsv
 	if [ -e $bartfile ]; then
 		nlines=`cat $bartfile | wc -l`
 		if [ $nlines -gt 1 ]; then
 			# You need BIDSto3col on your computer. Try git clone https://github.com/bids-standard/bidsutils
-			bash /data/tools/bidsutils/BIDSto3col/BIDSto3col.sh $bartfile ${output}/run-0${run}
+			bash /ZPOOL/data/tools/BIDSto3col.sh $bartfile ${output}/run-0${run}
 		else
 			echo "not enough lines: ${bartfile}" >> $logs/badformat_tsv.log
 		fi
